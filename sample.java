cd5dfc6d7f17ed4d2f1b87dd6c527b599baedeb8
Prathamesh Walke <prathameshwalke@macbookpro.mynetworksettings.com>
2025-04-07 21:39:05 -0400

    Solution for Palindrome LinkedList

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(N) N is the number of TreeNodes
-// Space Complexity : O(N) Recursion stack
+// Space Complexity : O(h) Recursion stack - height of the tree
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
@@ -29,5 +29,65 @@ class Solution {
 
         return 1 + Math.max(leftTree,rightTree);
 
+    }
+}
+
+// Time Complexity : O(N) - N is the nodes in LinkedList
+// Space Complexity : O(1) Recursion stack - height of the tree
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+// To solve this in one pass we travel to middle of the list and the from middle node to the last node we reverse the list
+// Then start comparing head to the reversed linked list
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head == null || head.next == null) return true;
+
+        ListNode middleNode = findMiddleOfList(head);
+        ListNode reverseNode = reverseList(middleNode);
+
+        return compareLists(reverseNode , head);
+    }
+    // 1 2 2 1
+    private ListNode findMiddleOfList (ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverseList(ListNode head){
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    private boolean compareLists(ListNode prev, ListNode head){
+
+        while(prev != null){
+            if(prev.val != head.val){
+                return false;
+            }
+            prev = prev.next;
+            head = head.next;
+        }
+        return true;
     }
 }
